32ecc25863c196e1e6b8aff0e9654aad05adcee8
Nhat Linh <linhnhatphuongnguyen@gmail.com>
2023-07-04 23:16:20 +0700

    Cherry pick commit ae3333fc15c230b8ffa21e0822e1a7bfda6a103a

---
+++ GroupProject_HRM_Library/DTOs/Bonus/BonusRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject_HRM_Library.DTOs.Bonus
+{
+    public class BonusRequest
+    {
+        [Required(ErrorMessage = "Employee ID is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Bonus Value is required")]
+        [DataType(DataType.Currency)]
+        [Range(0, 999999999999999.99)]
+        public decimal BonusValue { get; set; }
+
+        [Required(ErrorMessage = "Timestamp is required")]
+        public DateTime Timestamp { get; set; }
+
+        [Required(ErrorMessage = "Employee ID is required")]
+        public int EmployeeID { get; set; }
+    }
+}
---
+++ HRM_404NOTFOUND_API/Controllers/BonusController.cs
@@ -0,0 +1,53 @@
+﻿using GroupProject_HRM_Library.DTOs.Bonus;
+using GroupProject_HRM_Library.Errors;
+using GroupProject_HRM_Library.Exceptions;
+using GroupProject_HRM_Library.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GroupProject_HRM_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BonusController : ControllerBase
+    {
+        private IBonusRepository _bonusRepository;
+
+        public BonusController(IBonusRepository bonusRepository)
+        {
+            this._bonusRepository = bonusRepository;
+        }
+
+        [HttpPost, ActionName("Post Bonus")]
+        public async Task<IActionResult> PostBonusAsync([FromBody] BonusRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = new List<ErrorDetail>();
+                foreach (var pair in ModelState)
+                {
+                    if (pair.Value.Errors.Count > 0)
+                    {
+                        ErrorDetail errorDetail = new ErrorDetail()
+                        {
+                            FieldNameError = pair.Key,
+                            DescriptionError = pair.Value.Errors.Select(error => error.ErrorMessage).ToList()
+                        };
+                        errors.Add(errorDetail);
+                    }
+                }
+
+                var message = JsonConvert.SerializeObject(errors);
+                throw new BadRequestException(message);
+
+            }
+
+            await this._bonusRepository.CreateBonusAsync(request);
+            return Ok(new
+            {
+                Success = true,
+                Data = "Created bonus successfully."
+            });
+        }
+    }
+}
